5cd86b709ce984caf0e13d44d00ca3c90da78c47
Mirza Abdullah Baig <abaig7921gmail.com>
2023-07-23 09:11:10 +0500

    Task 02 Done

--- Task 02/Task 02.js
+++ Task 02/Task 02.js
@@ -1,1 +1,11 @@
-// Task 02:
+// Task 02: Write a program to store 3 student names in an array.Take another array to store score of these three students. Assume that total marks are 500 for each student, display the scores & percentages of students:
+
+student = ["Jalib", "Hamza", "Bilal"]
+score = [225, 195, 180]
+var totalmarks = 300;
+
+
+for (var i = 0; i < score.length; i++) {
+    per = score[i] / totalmarks * 100;
+    document.write(`Score of ${student[i]} is ${score[i]}. Percentage: ${per} <br/> <br/>`)
+}
