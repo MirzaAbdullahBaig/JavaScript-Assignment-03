d19baf5a8e1f1e99a9397c3b315c5f4a755caf48
Mirza Abdullah Baig <abaig7921gmail.com>
2023-07-23 07:51:11 +0500

    Task 01 Done

--- Task 01/Task 01.js
+++ Task 01/Task 01.js
@@ -1,1 +1,6 @@
-// Task 01:
+// Task 01; Declare and Initialize an array and store available education qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS, M.Phil., PhD).
+
+eductaion = ["1) SSC", "2) HSC", "3) BCS", "4) BS", "5) BCOM", "6) MS", "7) M.Phil", "8) PhD"];
+
+console.log("Eductaion Qualifications in Pakistan")
+console.log(eductaion);
