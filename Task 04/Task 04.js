75fc806c5dbdc7dfa080f0c07ee1f8e74590163c
Mirza Abdullah Baig <abaig7921gmail.com>
2023-07-31 19:17:49 +0500

    Task 04 Done

--- Task 04/Task 04.js
+++ Task 04/Task 04.js
@@ -1,1 +1,12 @@
-// Task 04:
+// Task 04: Write a program to initialize an array with city names. Copy 3 array elements from cities array to selected cities array.
+
+var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+
+document.write("<b> <h1> Cities List: <br/>");
+document.write(`<h3> ${cities} <br/>`);
+
+var selectedCitiesList1 = cities[2];
+var selectedCitiesList2 = cities[3];
+
+document.write("<b> <h1> Selected Cities List: <br/>");
+document.write(`<h3> ${selectedCitiesList1},${selectedCitiesList2} <br/>`);
