657ae34cb6dfaabed8a30cbab056164b5155c418
Mirza Abdullah Baig <abaig7921gmail.com>
2023-08-01 14:07:40 +0500

    Task 05 Done

--- Task 05/Task 05.js
+++ Task 05/Task 05.js
@@ -1,1 +1,8 @@
-// Task 05:
+// Task 05: Write a JavaScript program to remove all duplicate items from an array
+// Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+// Sample Output : [3, ‘a’, 2, 4, 9]
+
+var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+let singleChars = [...new Set(arr1)];
+
+document.write(`<b> ${singleChars}`);
